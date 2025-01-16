51e9c14fc90d007f8eb1d767bb90308f7096d2b4
suburban-loner <trent8423@gmail.com>
2025-01-16 13:05:20 +0200

    added loading page, changed characters to races.

--- app/admin/create-wiki-page/page.jsx
+++ app/admin/create-wiki-page/page.jsx
@@ -95,7 +95,7 @@ export default function CreateWikiPage() {
               <SelectValue placeholder="Select a category" />
             </SelectTrigger>
             <SelectContent>
-              <SelectItem value="characters">Characters</SelectItem>
+              <SelectItem value="races">Races</SelectItem>
               <SelectItem value="continents">Continents</SelectItem>
             </SelectContent>
           </Select>
