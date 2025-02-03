115e3e31378290b3338d4d71a42992d8f4c5217e
suburban-loner <teboho.cm02@gmail.com>
2025-02-03 18:59:49 +0200

    fixed home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,17 @@
 import Link from "next/link";
 
+import { Button } from "../components/ui/button";
+
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center text-center bg-white text-gray-700">
+    <div className="flex flex-col items-center text-center bg-white text-gray-700">
       <div>
-        <h1 className="flex text-4xl font-bold mb-4">Welcome to Ezra DnD</h1>
+        <h1 className="flex text-6xl font-bold mb-4">Welcome to Vizima</h1>
         <p className="text-lg mb-8">A Dungeons and Dragons campaign manager.</p>
       </div>
-      <div className="flex p-4 rounded-md mb-8">
-        <div className="mr-4 border-2 border-gray-400">
+      <div className="flex p-4 rounded-md mb-8 justify-center text-left">
+        <div className="flex-1 mr-2 border-4 border-gray-400 rounded-md p-2">
+          <h2 className="flex text-2xl font-bold mb-2">Welcome new players</h2>
           <p>
             Welcome to the world of Vīlībāzma, a world once, and still, seen as
             the battleground of the gods, where their never-ending conflict
@@ -23,30 +26,57 @@ export default function Home() {
             vanquished.
           </p>
         </div>
-        <div className="ml-4">
-          <h3>Races:</h3>
+        <div className="flex-1 border-4 mr-2 ml-2 px-4 rounded-md border-gray-400">
+          <h2 className="flex text-2xl font-bold mb-2">Races</h2>
           <ul>
-            <li>Human</li>
-            <li>Elf</li>
-            <li>Dwarf</li>
-            <li>Orc</li>
-            <li>Dragons</li>
-            <li>Dark Elves</li>
+            <li>
+              <li>
+                <Button asChild variant="link" className="p-2">
+                  <Link href="/human">Dark Elves</Link>
+                </Button>
+              </li>
+              <li>
+                <Button asChild variant="link" className="p-2">
+                  <Link href="/human">Dragons</Link>
+                </Button>
+              </li>
+              <li>
+                <Button asChild variant="link" className="p-2">
+                  <Link href="/human">Dwarves</Link>
+                </Button>
+              </li>
+              <li>
+                <Button asChild variant="link" className="p-2">
+                  <Link href="/human">Elves</Link>
+                </Button>
+              </li>
+              <Button asChild variant="link" className="p-2">
+                <Link href="/human">Humans</Link>
+              </Button>
+            </li>
+            <li>
+              <Button asChild variant="link" className="p-2">
+                <Link href="/human">Orcs</Link>
+              </Button>
+            </li>
+          </ul>
+        </div>
+        <div className="flex-1 mr-2 ml-2 rounded-md px-4 border-4 border-gray-400">
+          <h2 className="flex text-2xl font-bold mb-2">Continents</h2>
+          <ul>
+            <li>
+              <Button asChild variant="link" className="p-2">
+                <Link href="/human">Alfar Saltus</Link>
+              </Button>
+            </li>
+            <li>
+              <Button asChild variant="link" className="p-2">
+                <Link href="/human">Oloklerious</Link>
+              </Button>
+            </li>
           </ul>
         </div>
       </div>
-      <div className="flex p-4 rounded-md mb-8">
-        <h3>Continents:</h3>
-        <ul>
-          <li>oloklerious</li>
-          <li>alfar saltus</li>
-        </ul>
-      </div>
-      <Link href="/dashboard" passHref>
-        <button className="px-5 py-2.5 text-base bg-gray-400 text-white border-0 rounded-md cursor-pointer">
-          Go to Dashboard
-        </button>
-      </Link>
     </div>
   );
 }
