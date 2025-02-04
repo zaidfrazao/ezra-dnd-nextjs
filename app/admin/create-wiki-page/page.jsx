5f560fb2269926832bf9ffcc683201b3de75c7ff
Zaid Frazao <zaidfrazao@gmail.com>
2025-02-04 16:12:17 +0200

    Minor fix to create wiki form

--- app/admin/create-wiki-page/page.jsx
+++ app/admin/create-wiki-page/page.jsx
@@ -141,7 +141,7 @@ export default function CreateWikiPage() {
               <SelectValue placeholder="Select a category" />
             </SelectTrigger>
             <SelectContent>
-              <SelectItem value="races">Races</SelectItem>
+              <SelectItem value="characters">Characters</SelectItem>
               <SelectItem value="continents">Continents</SelectItem>
             </SelectContent>
           </Select>
