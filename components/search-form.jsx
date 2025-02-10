fd4d59706c203f4a961af910ebce44ade7c25100
Zaid Frazao <zaidfrazao@gmail.com>
2025-02-10 17:39:03 +0200

    Make changes to app bar loading state

--- components/search-form.jsx
+++ components/search-form.jsx
@@ -15,7 +15,6 @@ import {
 } from "@/components/ui/sidebar";
 
 export function SearchForm({ onSearch, ...props }) {
-  const {isLoading} = props;
   const [search, setSearch] = useState("");
   const router = useRouter();
 
@@ -47,22 +46,6 @@ export function SearchForm({ onSearch, ...props }) {
       console.error("Error searching Firestore:", error);
     }
   };
-
-  if (isLoading) {
-    return (
-      <form {...props} onSubmit={handleSubmit}>
-        <SidebarGroup className="py-0">
-          <SidebarGroupContent className="relative">
-            <Label htmlFor="search" className="sr-only">
-              Search
-            </Label>
-            <Skeleton className="h-5 w-40 rounded-md bg-zinc-300 my-2" />
-            <Search className="pointer-events-none absolute left-2 top-1/2 size-4 -translate-y-1/2 select-none opacity-50" />
-          </SidebarGroupContent>
-        </SidebarGroup>
-      </form>
-    );
-  }
 
   return (
     <form {...props} onSubmit={handleSubmit}>
